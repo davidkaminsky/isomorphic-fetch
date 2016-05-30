10be15911947396dccaf1bb851445303637fee3c
David Kaminsky <oursfs@gmail.com>
2016-05-30 11:09:30 -0700

    Added ability to pass through cookie.

--- fetch-npm-node.js
+++ fetch-npm-node.js
@@ -1,11 +1,21 @@
 "use strict";
 
 var realFetch = require('node-fetch');
+
+var cookie = '';
+
 module.exports = function(url, options) {
 	if (/^\/\//.test(url)) {
 		url = 'https:' + url;
 	}
+	options = options || {};
+	options.headers = options.headers || {};
+	options.headers.cookie = cookie;
 	return realFetch.call(this, url, options);
+};
+
+module.exports.setCookie = function(c) {
+	cookie = c;
 };
 
 if (!global.fetch) {
@@ -14,3 +24,4 @@ if (!global.fetch) {
 	global.Headers = realFetch.Headers;
 	global.Request = realFetch.Request;
 }
+
