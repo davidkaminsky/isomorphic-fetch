840a298019e4f5822879714355b75770890e3189
David Kaminsky <oursfs@gmail.com>
2016-05-30 21:35:01 -0700

    Redirect to root on 401 from fetch.

--- fetch-npm-node.js
+++ fetch-npm-node.js
@@ -3,6 +3,7 @@
 var realFetch = require('node-fetch');
 
 var cookie = '';
+var res = null;
 
 module.exports = function(url, options) {
 	if (/^\/\//.test(url)) {
@@ -11,11 +12,23 @@ module.exports = function(url, options) {
 	options = options || {};
 	options.headers = options.headers || {};
 	options.headers.cookie = cookie;
-	return realFetch.call(this, url, options);
+	return realFetch.call(this, url, options)
+		.then(function (response) {
+			// If we see a 401, then redirect on the server to root.
+			if (response.status == 401 && res) {
+				res.redirect('/');
+			}
+			
+			return response;
+		});
 };
 
 module.exports.setCookie = function(c) {
 	cookie = c;
+};
+
+module.exports.setResponse = function(r) {
+	res = r;
 };
 
 if (!global.fetch) {
